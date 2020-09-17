f3f03ae1e6f361ab66fe83ddf90a093f9b82c83b
cristinaquintanilla <70345076+cristinaquintanilla@users.noreply.github.com>
2020-09-17 14:55:26 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Ec")
+    energiacinetica = masa * velocidad / 2
+    basic.showString("Ec=")
+    basic.showNumber(energiacinetica)
+})
+let energiacinetica = 0
+let velocidad = 0
+let masa = 0
+basic.showString("Cristina Quintanilla - A01286364")
+masa = 17.78
+velocidad = 15
